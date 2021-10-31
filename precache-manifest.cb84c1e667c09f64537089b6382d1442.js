88f881335e7b5ad78584e53498334d457429dcfd
foresightJunyu <373034891@qq.com>
2021-10-31 23:47:14 +0800

    deploy

---
+++ precache-manifest.cb84c1e667c09f64537089b6382d1442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc2a42ada7e8aba60cf",
+    "url": "/website-cnode/css/app.2e69df79.css"
+  },
+  {
+    "revision": "102a9f484be00a51ebcf5157ca108dbe",
+    "url": "/website-cnode/img/loading.102a9f48.gif"
+  },
+  {
+    "revision": "8ac5ca4dd2bf00fb137f2459bd190a9c",
+    "url": "/website-cnode/index.html"
+  },
+  {
+    "revision": "5fc2a42ada7e8aba60cf",
+    "url": "/website-cnode/js/app.3f2802fe.js"
+  },
+  {
+    "revision": "1de3d2b365ad4d442e24",
+    "url": "/website-cnode/js/chunk-vendors.044f1b7b.js"
+  },
+  {
+    "revision": "53656bdc78c24067349596cab3d4c767",
+    "url": "/website-cnode/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/website-cnode/robots.txt"
+  }
+]);
